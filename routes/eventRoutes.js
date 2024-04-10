bd4d02c6d7df20bf20bcdd7f7b0bf774897376f3
sourav0809 <spathak7431@gmail.com>
2024-04-10 13:41:26 +0530

    fetch event api added

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,11 +4,14 @@ const { validate } = require('../middlewares/validate');
 const eventValidation = require('../validations/eventValidation');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+
 router.post(
   '/createevent',
   auth,
   validate(eventValidation.createEvent),
   eventController.createEvent
 );
+
+router.get('/getevents', auth, eventController.getEvents);
 
 module.exports = router;
